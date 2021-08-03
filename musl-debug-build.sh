15813abde1a84448d939ca77a7f26cc20b6585c0
gianluigi <gianluigi@exein.io>
2021-08-03 10:30:42 +0200

    Updated dependencies Added instruction for i686-musl build

--- musl-debug-build.sh
+++ musl-debug-build.sh
@@ -1,2 +1,10 @@
 # Statically build the executable in debug mode
 cargo build --target x86_64-unknown-linux-musl
+
+
+# i686-musl
+
+# docker pull messense/rust-musl-cross:i686-musl
+# alias rust-musl-builder='docker run --rm -it -v "$(pwd)":/home/rust/src  messense/rust-musl-cross:i686-musl'
+# rust-musl-builder cargo build --release
+
